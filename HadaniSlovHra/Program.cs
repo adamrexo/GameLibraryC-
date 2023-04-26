da52dfcd0a1e4e4ef558e43cc4c18f5c48c67b60
adamrexo <127039714+adamrexo@users.noreply.github.com>
2023-04-26 20:36:13 +0200

    Změny a přidání bankomatu

--- HadaniSlovHra/Program.cs
+++ HadaniSlovHra/Program.cs
@@ -28,6 +28,7 @@ namespace GameLib
                 Console.WriteLine("");
                 Console.ForegroundColor = ConsoleColor.Cyan;
                 Console.WriteLine("0 = Hádání slov    1 = Kámen, Nůžky, Papír    2 = Hoď si kostkou!");
+                Console.WriteLine("3 = ATM Simulator    4 = Coming Soon    5 = Coming Soon");
                 Console.WriteLine("9 = Přeji si ukončit aplikaci");
                 Console.WriteLine("");
                 Console.WriteLine("Vítejte v GameLib! Napište číslo dle menu, abyste si zvolili hru, kterou si chcete zahrát!");
@@ -57,6 +58,9 @@ namespace GameLib
                         break;
                     case 2:
                         Kostky();
+                        break;
+                    case 3:
+                        ATM();
                         break;
                     case 9:
                         Console.Clear();
@@ -379,6 +383,178 @@ namespace GameLib
         }
         //
         // KOSTKY KONEC
+        //
+
+        //
+        // ATM
+        //
+
+        static void ATM()
+        {
+            Console.ResetColor();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+
+            ATMMenu();
+
+            Console.Clear();
+
+            string jmeno = "n";
+            using (StreamReader sr = new StreamReader(@"jmeno.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    jmeno = s;
+                }
+            }
+            string prijmeni = "n";
+            using (StreamReader sr = new StreamReader(@"prijmeni.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    prijmeni = s;
+                }
+            }
+            double penize = 0;
+            using (StreamReader sr = new StreamReader(@"penize.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    penize = double.Parse(s);
+                }
+            }
+            int pin = 0000;
+            using (StreamReader sr = new StreamReader(@"pin.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    pin = int.Parse(s);
+                }
+            }
+
+            Console.WriteLine("Vítej v bankomatu!");
+            string zj = "Jmeno";
+            bool Vstup1 = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Zadej prosím své jméno:");
+                zj = Console.ReadLine();
+                if (zj == jmeno)
+                {
+                    Vstup1 = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zadal jsi špatné jméno! Zkus to znovu.");
+                    Console.ResetColor();
+                }
+            } while (Vstup1 == false);
+
+            string zp = "Prijmeni";
+            bool Vstup2 = false;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Zadej prosím své příjmení:");
+                zp = Console.ReadLine();
+                if (zp == prijmeni)
+                {
+                    Vstup2 = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zadal jsi špatné příjmení! Zkus to znovu.");
+                    Console.ResetColor();
+                }
+            } while (Vstup2 == false);
+
+            int zpok = 3;
+            bool Vstup3 = false;
+            int zpin = 1;
+            do
+            {
+                Console.ForegroundColor = ConsoleColor.Blue;
+                Console.WriteLine("Zadej svůj pin:");
+                try
+                {
+                    zpin = int.Parse(Console.ReadLine());
+                }
+                catch (Exception)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zadali jste neplatný vstup! Zkuste to znovu.");
+                    Console.ResetColor();
+                }
+                if (zpin == pin)
+                {
+                    Vstup3 = true;
+                }
+                else
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Zadali jste špatný pin!");
+                    zpok--;
+                    Console.WriteLine("Zbývá ti následující počet pokusu: " + zpok);
+                    if (zpok == 0)
+                    {
+                        Console.WriteLine("Bohužel, vyčerpal jsi maximální počet pokusů.");
+                        Console.WriteLine("Zkus to příště!");
+                        Console.ReadKey();
+                        Environment.Exit(0);
+                    }
+                    Console.ResetColor();
+                }
+            } while (Vstup3 == false);
+
+            Console.WriteLine("Vítej {0} {1} v ATM!", jmeno, prijmeni);
+            Console.WriteLine("Zadej akci, kterou chceš provést:");
+            Console.WriteLine("0 = Výběr    1 = Vklad");
+            Console.WriteLine("2 = Změnit PIN    3 = Opustit bankomat");
+            string Vstup = Console.ReadLine();
+            switch (Vstup)
+            {
+                default:
+                    Console.WriteLine("Zadal jsi špatnou hodnotu!");
+                    break;
+            }
+
+            Console.ReadKey();
+        }
+
+        static void ATMMenu()
+        {
+            Console.ResetColor();
+            Console.Clear();
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.WriteLine("Vítej v ATM Simulátoru! Uživatelské jméno a příjmení v simulátoru je:");
+            using (StreamReader sr = new StreamReader(@"jmeno.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.Write(s + " ");
+                }
+            }
+            using (StreamReader sr = new StreamReader(@"prijmeni.txt"))
+            {
+                string s;
+                while ((s = sr.ReadLine()) != null)
+                {
+                    Console.WriteLine(s);
+                }
+            }
+            Console.WriteLine("Stiskni libovolné tlačítko pro pokračování do simulátoru.");
+            Console.ReadKey();
+        }
+        //
+        // ATM Konec
         //
     }
 }
